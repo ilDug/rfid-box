afc344b0f04883e3b7a4e986b8984e393b847158
ilDug <dognini.marco@gmail.com>
2024-03-26 17:17:40 +0100

    addin library

--- rfid-box.ino
+++ rfid-box.ino
@@ -1,3 +1,14 @@
+/**
+ * @file rfid-box.ino
+ * @brief RFID Box
+ * @details This is the main file for the RFID Box project. It includes the setup and loop functions.
+ * 
+*/
+
+#include <MFRC522.h> // https://github.com/miguelbalboa/rfid
+#include "dag-button.h"
+
+
 
 void setup()
 {
